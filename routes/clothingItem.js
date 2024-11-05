e8b32864003627c54a7dde19f9576c2a04cf7878
Andy <andywickoren@gmail.com>
2024-11-05 14:49:47 -0600

    add auth middleware to clothingItem routes

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const auth = require("../middleware/auth");
 
 const {
   createItem,
@@ -8,10 +9,10 @@ const {
   dislikeItem,
 } = require("../controllers/clothingItem");
 
-router.post("/", createItem);
+router.post("/", auth, createItem);
 router.get("/", getItems);
-router.delete("/:itemId", deleteItem);
-router.put("/:itemId/likes", likeItem);
-router.delete("/:itemId/likes", dislikeItem);
+router.delete("/:itemId", auth, deleteItem);
+router.put("/:itemId/likes", auth, likeItem);
+router.delete("/:itemId/likes", auth, dislikeItem);
 
 module.exports = router;
